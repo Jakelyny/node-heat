777a141021aebf4baf448a898e75e20274d8dcd6
Jakelyny <jakelynysousa.a@gmail.com>
2021-10-20 20:48:20 -0300

    .

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
+import "dotenv/config";
 import express, { response } from "express";
 
 const app = express();
 
 app.get("/github", (request, response) => {
-  response.redirect('https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}')
-})
+  response.redirect('https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}'
+  );
+});
 
 app.listen(4000, () => console.log('🐓 Server is running on PORT 4000'));
