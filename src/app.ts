b5b8b545cfff340357ad5024c6aa5a85a9829f58
Jakelyny <jakelynysousa.a@gmail.com>
2021-10-20 20:39:06 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
+
+app.get("/github", (request, response) => {
+  response.redirect('https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}')
+})
 
 app.listen(4000, () => console.log('🐓 Server is running on PORT 4000'));
